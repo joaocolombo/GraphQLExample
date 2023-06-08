7bac3692c8ca7205b1553071a78b1056192daac0
Joao <joao.c@outlook.com>
2023-06-08 17:17:14 -0300

    simple

---
+++ GraphQLExample/Query.cs
@@ -0,0 +1,16 @@
+﻿namespace GraphQLExample;
+
+public class Query
+{
+    public string Hello(string name) => $"Hello {name}!";
+
+    public IEnumerable<Book> GetBooks() => new[]
+    {
+        new Book("The Fellowship of the Ring", new Author("J.R.R. Tolkien", "Bio")),
+        new Book("The Two Towers", new Author("J.R.R. Tolkien", "Bio")),
+        new Book("The Return of the King", new Author("J.R.R. Tolkien", "Bio")),
+    };
+}
+
+public record Author(string Name, string Bio);
+public record Book(string Title, Author Author);
